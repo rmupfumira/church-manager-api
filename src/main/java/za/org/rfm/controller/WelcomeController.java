659f8043babf181dbb11118fa3290cc67249b901
Russel Mupfumira <russel@rfm.org.za>
2018-05-09 15:42:43 +0200

    added the controller code

--- src/main/java/za/org/rfm/controller/WelcomeController.java
+++ src/main/java/za/org/rfm/controller/WelcomeController.java
@@ -1,15 +1,18 @@
 package za.org.rfm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 import java.util.Map;
 
+import za.org.rfm.entity.Guest;
 import za.org.rfm.entity.Assembly;
 import za.org.rfm.service.AssemblyService;
+import za.org.rfm.service.EmailService;
 
 @Controller
 public class WelcomeController {
@@ -17,11 +20,21 @@ public class WelcomeController {
     @Autowired
     private AssemblyService assemblyService;
 
+    @Autowired
+    private EmailService emailService;
+
     @RequestMapping("/")
     public String welcome(Map<String, Object> model) {
         List<Assembly> assemblyList = assemblyService.getAllAssemblies();
         model.put("assemblyList", assemblyList);
+        model.put("urlBase", assemblyList);
         return "welcome";
+    }
+    @PostMapping("/guest")
+    public String sendWebsiteEnquiryEmail(@RequestBody Guest guest){
+        emailService.sendWebSiteEmail(guest);
+
+        return "Done!";
     }
 
 }
