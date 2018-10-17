f893d5e14c44b95b2bf5859925db343e9f8da125
Russel Mupfumira <russel@jmr.co.za>
2018-10-17 16:50:24 +0200

    added velocity

--- src/main/java/za/org/rfm/MyConfiguration.java
+++ src/main/java/za/org/rfm/MyConfiguration.java
@@ -1,10 +1,17 @@
 package za.org.rfm;
 
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.velocity.VelocityEngineFactory;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.velocity.VelocityConfigurer;
+
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 public class MyConfiguration {
@@ -17,5 +24,15 @@ public class MyConfiguration {
                 registry.addMapping("/**");
             }
         };
+    }
+
+    @Bean
+    public VelocityEngine getVelocityEngine() throws VelocityException, IOException {
+        VelocityEngineFactory factory = new VelocityEngineFactory();
+        Properties props = new Properties();
+        props.put("resource.loader", "class");
+        props.put("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        factory.setVelocityProperties(props);
+        return factory.createVelocityEngine();
     }
 }
---
+++ src/main/java/za/org/rfm/config/MailConfig.java
@@ -0,0 +1,49 @@
+package za.org.rfm.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+public class MailConfig {
+
+    @Value("${mail.protocol}")  // this is to read variable from application.properties
+    private String mailProtocol;
+
+    @Value("${mail.smtp.host}")
+    private String host;
+
+    @Value("${mail.smtp.port}")
+    private Integer port;
+
+    @Value("${mail.support.username}")
+    private String userName;
+
+    @Value("${mail.support.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setProtocol(mailProtocol);
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(userName);
+        javaMailSender.setPassword(password);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "false");
+        return properties;
+    }
+}
