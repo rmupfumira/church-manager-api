efbdb96da5ea9f88039f8130716222a4becad507
Russel Mupfumira <russel@jmr.co.za>
2018-11-09 15:39:25 +0200

    added scheduling of tasks

--- src/main/java/za/org/rfm/MyConfiguration.java
+++ src/main/java/za/org/rfm/MyConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import java.util.Properties;
 
 @Configuration
+@EnableScheduling
 public class MyConfiguration {
 
 
